0ee1394015c06286876340129b186c8fdd9a03bb
AstMep <blanca_nieves130804@hotmail.com>
2025-09-29 00:21:19 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_Boletas/Menu_principal.Designer.cs
@@ -0,0 +1,443 @@
+﻿namespace Proyecto_Boletas
+{
+    partial class Menu_principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu_principal));
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            btn_inscripcion = new Button();
+            groupBox2 = new GroupBox();
+            label2 = new Label();
+            btn_capturaCalif = new Button();
+            groupBox3 = new GroupBox();
+            label4 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            groupBox4 = new GroupBox();
+            label5 = new Label();
+            label6 = new Label();
+            button2 = new Button();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            groupBox5 = new GroupBox();
+            label11 = new Label();
+            button3 = new Button();
+            groupBox6 = new GroupBox();
+            label12 = new Label();
+            label13 = new Label();
+            button4 = new Button();
+            panel1 = new Panel();
+            label14 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox5.SuspendLayout();
+            groupBox6.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.FromArgb(157, 101, 101);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(btn_inscripcion);
+            groupBox1.Location = new Point(80, 76);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(160, 186);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(41, 147);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 21);
+            label1.TabIndex = 4;
+            label1.Text = "Inscripción";
+            // 
+            // btn_inscripcion
+            // 
+            btn_inscripcion.BackColor = Color.FromArgb(212, 168, 133);
+            btn_inscripcion.BackgroundImageLayout = ImageLayout.Center;
+            btn_inscripcion.FlatStyle = FlatStyle.Popup;
+            btn_inscripcion.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_inscripcion.Image = (Image)resources.GetObject("btn_inscripcion.Image");
+            btn_inscripcion.Location = new Point(19, 9);
+            btn_inscripcion.Name = "btn_inscripcion";
+            btn_inscripcion.Size = new Size(121, 135);
+            btn_inscripcion.TabIndex = 3;
+            btn_inscripcion.UseVisualStyleBackColor = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = Color.FromArgb(157, 101, 101);
+            groupBox2.Controls.Add(label8);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(btn_capturaCalif);
+            groupBox2.Location = new Point(301, 76);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(160, 186);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(41, 144);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 21);
+            label2.TabIndex = 4;
+            label2.Text = "Captura de";
+            // 
+            // btn_capturaCalif
+            // 
+            btn_capturaCalif.BackColor = Color.FromArgb(212, 168, 133);
+            btn_capturaCalif.BackgroundImageLayout = ImageLayout.Center;
+            btn_capturaCalif.FlatStyle = FlatStyle.Popup;
+            btn_capturaCalif.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_capturaCalif.Image = (Image)resources.GetObject("btn_capturaCalif.Image");
+            btn_capturaCalif.Location = new Point(19, 9);
+            btn_capturaCalif.Name = "btn_capturaCalif";
+            btn_capturaCalif.Size = new Size(121, 135);
+            btn_capturaCalif.TabIndex = 3;
+            btn_capturaCalif.UseVisualStyleBackColor = false;
+            // 
+            // groupBox3
+            // 
+            groupBox3.BackColor = Color.FromArgb(157, 101, 101);
+            groupBox3.Controls.Add(label7);
+            groupBox3.Controls.Add(label4);
+            groupBox3.Controls.Add(label3);
+            groupBox3.Controls.Add(button1);
+            groupBox3.Location = new Point(514, 76);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(160, 186);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ControlLightLight;
+            label4.Location = new Point(30, 162);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 21);
+            label4.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(56, 147);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 21);
+            label3.TabIndex = 4;
+            label3.Click += label3_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(212, 168, 133);
+            button1.BackgroundImageLayout = ImageLayout.Center;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(19, 9);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 135);
+            button1.TabIndex = 3;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // groupBox4
+            // 
+            groupBox4.BackColor = Color.FromArgb(157, 101, 101);
+            groupBox4.Controls.Add(label10);
+            groupBox4.Controls.Add(label9);
+            groupBox4.Controls.Add(label5);
+            groupBox4.Controls.Add(label6);
+            groupBox4.Controls.Add(button2);
+            groupBox4.Location = new Point(80, 303);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(160, 186);
+            groupBox4.TabIndex = 4;
+            groupBox4.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ControlLightLight;
+            label5.Location = new Point(30, 162);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 21);
+            label5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.ControlLightLight;
+            label6.Location = new Point(34, 144);
+            label6.Name = "label6";
+            label6.Size = new Size(0, 21);
+            label6.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(212, 168, 133);
+            button2.BackgroundImageLayout = ImageLayout.Center;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.Location = new Point(19, 9);
+            button2.Name = "button2";
+            button2.Size = new Size(121, 135);
+            button2.TabIndex = 3;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = SystemColors.ControlLightLight;
+            label7.Location = new Point(36, 147);
+            label7.Name = "label7";
+            label7.Size = new Size(93, 21);
+            label7.TabIndex = 5;
+            label7.Text = "Estadisticas";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = SystemColors.ControlLightLight;
+            label8.Location = new Point(30, 162);
+            label8.Name = "label8";
+            label8.Size = new Size(110, 21);
+            label8.TabIndex = 5;
+            label8.Text = "Calificaciones";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = SystemColors.ControlLightLight;
+            label9.Location = new Point(30, 144);
+            label9.Name = "label9";
+            label9.Size = new Size(103, 21);
+            label9.TabIndex = 6;
+            label9.Text = "Información ";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = SystemColors.ControlLightLight;
+            label10.Location = new Point(38, 162);
+            label10.Name = "label10";
+            label10.Size = new Size(93, 21);
+            label10.TabIndex = 7;
+            label10.Text = "de Bitacora";
+            // 
+            // groupBox5
+            // 
+            groupBox5.BackColor = Color.FromArgb(157, 101, 101);
+            groupBox5.Controls.Add(label11);
+            groupBox5.Controls.Add(button3);
+            groupBox5.Location = new Point(301, 303);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(160, 186);
+            groupBox5.TabIndex = 5;
+            groupBox5.TabStop = false;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.ForeColor = SystemColors.ControlLightLight;
+            label11.Location = new Point(19, 147);
+            label11.Name = "label11";
+            label11.Size = new Size(132, 21);
+            label11.TabIndex = 4;
+            label11.Text = "Edición de Datos";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(212, 168, 133);
+            button3.BackgroundImageLayout = ImageLayout.Center;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.Location = new Point(19, 9);
+            button3.Name = "button3";
+            button3.Size = new Size(121, 135);
+            button3.TabIndex = 3;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // groupBox6
+            // 
+            groupBox6.BackColor = Color.FromArgb(157, 101, 101);
+            groupBox6.Controls.Add(label12);
+            groupBox6.Controls.Add(label13);
+            groupBox6.Controls.Add(button4);
+            groupBox6.Location = new Point(514, 303);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(160, 186);
+            groupBox6.TabIndex = 6;
+            groupBox6.TabStop = false;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.ForeColor = SystemColors.ControlLightLight;
+            label12.Location = new Point(48, 162);
+            label12.Name = "label12";
+            label12.Size = new Size(64, 21);
+            label12.TabIndex = 5;
+            label12.Text = "Boletas";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label13.ForeColor = SystemColors.ControlLightLight;
+            label13.Location = new Point(36, 144);
+            label13.Name = "label13";
+            label13.Size = new Size(76, 21);
+            label13.TabIndex = 4;
+            label13.Text = "Envio de ";
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(212, 168, 133);
+            button4.BackgroundImageLayout = ImageLayout.Center;
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.Location = new Point(19, 9);
+            button4.Name = "button4";
+            button4.Size = new Size(121, 135);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(39, 66, 89);
+            panel1.Controls.Add(label14);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 48);
+            panel1.TabIndex = 7;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Cascadia Code", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label14.ForeColor = SystemColors.ControlLightLight;
+            label14.Location = new Point(220, 9);
+            label14.Name = "label14";
+            label14.Size = new Size(406, 32);
+            label14.TabIndex = 6;
+            label14.Text = "Bienvenido al Sistema FAY JC";
+            // 
+            // Menu_principal
+            // 
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BackColor = Color.FromArgb(181, 131, 120);
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(800, 517);
+            Controls.Add(panel1);
+            Controls.Add(groupBox6);
+            Controls.Add(groupBox5);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Menu_principal";
+            Text = "Menu_principal";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            groupBox5.ResumeLayout(false);
+            groupBox5.PerformLayout();
+            groupBox6.ResumeLayout(false);
+            groupBox6.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private GroupBox groupBox1;
+        private Button btn_inscripcion;
+        private Label label1;
+        private GroupBox groupBox2;
+        private Label label2;
+        private Button btn_capturaCalif;
+        private GroupBox groupBox3;
+        private Label label3;
+        private Button button1;
+        private Label label4;
+        private GroupBox groupBox4;
+        private Label label5;
+        private Label label6;
+        private Button button2;
+        private Label label8;
+        private Label label7;
+        private Label label9;
+        private Label label10;
+        private GroupBox groupBox5;
+        private Label label11;
+        private Button button3;
+        private GroupBox groupBox6;
+        private Label label12;
+        private Label label13;
+        private Button button4;
+        private Panel panel1;
+        private Label label14;
+    }
+}
---
+++ Proyecto_Boletas/Menu_principal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Boletas
+{
+    public partial class Menu_principal : Form
+    {
+        public Menu_principal()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
